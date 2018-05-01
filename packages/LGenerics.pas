1462f2c034f79f2621da225c22fb63a41b298946
avk959 <rbyter@mail.ru>
2018-05-01 23:10:19 +0700

    naming changes

--- packages/LGenerics.pas
+++ packages/LGenerics.pas
@@ -10,7 +10,8 @@ interface
 uses
   LGUtils, LGCustomContainer, LGHash, LGFunction, LGArrayHelpers, LGHelpers, LGMiscUtils, LGStack, LGStrConst, 
   LGPriorityQueue, LGDeque, LGHashSet, LGHashMultiSet, LGHashMap, LGTreeSet, LGTreeMultiSet, LGTreeMap, 
-  LGAvlTree, LGStrHelpers, LGHashTable, LGVector, LGMultiMap, LGBiMap, LGSortedList, LGAsync, LGQueue, LGTable;
+  LGAvlTree, LGStrHelpers, LGHashTable, LGVector, LGMultiMap, LGBiMap, LGSortedList, LGAsync, LGQueue, 
+  LGTable2D;
 
 implementation
 
