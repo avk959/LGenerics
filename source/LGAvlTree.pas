b5098e53aa060e8b4bc0329f29653b9ccbae1cd7
avk959 <rbyter@mail.ru>
2018-05-09 22:18:07 +0700

    jit is also not without errors

--- source/LGAvlTree.pas
+++ source/LGAvlTree.pas
@@ -45,53 +45,6 @@ type
   type
     PNode = ^TNode;
 
-    TNode = record{****************************************************************************
-*                                                                           *
-*   This file is part of the LGenerics package.                             *
-*   Generic AVL tree implementations for internal use.                      *
-*   (see https://en.wikipedia.org/wiki/AVL_tree)                            *
-*                                                                           *
-*   Copyright(c) 2018 A.Koverdyaev(avk)                                     *
-*                                                                           *
-*   This code is free software; you can redistribute it and/or modify it    *
-*   under the terms of the Apache License, Version 2.0;                     *
-*   You may obtain a copy of the License at                                 *
-*     http://www.apache.org/licenses/LICENSE-2.0.                           *
-*                                                                           *
-*  Unless required by applicable law or agreed to in writing, software      *
-*  distributed under the License is distributed on an "AS IS" BASIS,        *
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
-*  See the License for the specific language governing permissions and      *
-*  limitations under the License.                                           *
-*                                                                           *
-*****************************************************************************}
-unit LGAvlTree;
-
-{$mode objfpc}{$H+}
-{$INLINE ON}
-{$MODESWITCH ADVANCEDRECORDS}
-{$MODESWITCH NESTEDPROCVARS}
-{$PACKRECORDS DEFAULT}
-
-interface
-
-uses
-
-  SysUtils,
-  LGUtils,
-  {%H-}LGHelpers,
-  LGStrConst;
-
-{.$DEFINE AVLTREE_ENABLE_PAGEDNODEMANAGER}//if uncomment define, will use TGPageNodeManager
-type
-
-  { TGCustomAvlTree }
-
-  generic TGCustomAvlTree<TKey, TEntry> = class abstract
-  public
-  type
-    PNode = ^TNode;
-
     TNode = record
     private
     {$IFNDEF CPU16}
