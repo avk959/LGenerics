b6b5876ab0d3592097028fd11133c3401e8be3a1
avk959 <rbyter@mail.ru>
2018-05-19 23:31:25 +0700

    gradual expansion of functionality

--- source/LGSimpleDGraph.pas
+++ source/LGSimpleDGraph.pas
@@ -45,10 +45,139 @@ type
         class function Equal([const[ref]] L, R: TVertex): Boolean; }
   generic TGSimpleSparseDGraph<TVertex, TEdgeData, TVertexEqRel> = class(
     specialize TGCustomSimpleSparseGraph<TVertex, TEdgeData, TVertexEqRel>)
-
+  protected
+  class var
+    CFData: TEdgeData;
+    procedure DoRemoveVertex(aIndex: SizeInt);
+    function  DoAddEdge(aSrc, aDst: SizeInt; aData: TEdgeData): Boolean;
+    function  DoRemoveEdge(aSrc, aDst: SizeInt): Boolean;
+  public
+    function  RemoveVertex(constref v: TVertex): Boolean; inline;
+    function  RemoveVertexI(aIndex: SizeInt): Boolean;
+    function  AddEdge(constref aSrc, aDst: TVertex; aData: TEdgeData): Boolean;
+    function  AddEdge(constref aSrc, aDst: TVertex): Boolean; inline;
+    function  AddEdgeI(aSrc, aDst: SizeInt; aData: TEdgeData): Boolean;
+    function  AddEdgeI(aSrc, aDst: SizeInt): Boolean; inline;
+    function  RemoveEdge(constref aSrc, aDst: TVertex): Boolean; inline;
+    function  RemoveEdgeI(aSrc, aDst: SizeInt): Boolean;
   end;
 
 implementation
+{$B-}{$COPERATORS ON}
+
+{ TGSimpleSparseDGraph }
+
+procedure TGSimpleSparseDGraph.DoRemoveVertex(aIndex: SizeInt);
+var
+  I, J: SizeInt;
+  pV: ^TVertexItem;
+  p: ^TAdjItem;
+  CurrEdges: TVertexItem.TAdjItemArray;
+begin
+  FEdgeCount -= FVertexList.ItemRefs[aIndex]^.Count;
+  FConnectedValid := False;
+  for p in FVertexList.ItemRefs[aIndex]^ do
+    Dec(FVertexList.ItemRefs[p^.Destination]^.Tag);
+  FVertexList.Delete(aIndex);
+  for I := 0 to Pred(FVertexList.Count) do
+    begin
+      CurrEdges := FVertexList.ItemRefs[I]^.ToArray;
+      FVertexList.FNodeList[I].Item.MakeEmpty;
+      for J := 0 to System.High(CurrEdges) do
+        begin
+          if CurrEdges[J].Destination <> aIndex then
+            begin
+              if CurrEdges[J].Destination > aIndex then
+                Dec(CurrEdges[J].Destination);
+              FVertexList.ItemRefs[I]^.Add(CurrEdges[J]);
+            end;
+        end;
+    end;
+end;
+
+function TGSimpleSparseDGraph.DoAddEdge(aSrc, aDst: SizeInt; aData: TEdgeData): Boolean;
+var
+  p: ^TAdjItem;
+begin
+  if aSrc = aDst then
+    exit(False);
+  Result := not FVertexList.ItemRefs[aSrc]^.FindOrAdd(aDst, p);
+  if Result then
+    begin
+      p^.Destination := aDst;
+      p^.Data := aData;
+      Inc(FVertexList.ItemRefs[aDst]^.Tag);
+      Inc(FEdgeCount);
+      FConnectedValid := False;
+    end;
+end;
+
+function TGSimpleSparseDGraph.DoRemoveEdge(aSrc, aDst: SizeInt): Boolean;
+begin
+  if aSrc = aDst then
+    exit(False);
+  Result := FVertexList.ItemRefs[aSrc]^.Remove(aDst);
+  if Result then
+    begin
+      Dec(FVertexList.ItemRefs[aDst]^.Tag);
+      Dec(FEdgeCount);
+      FConnectedValid := False;
+    end;
+end;
+
+function TGSimpleSparseDGraph.RemoveVertex(constref v: TVertex): Boolean;
+begin
+  Result := RemoveVertexI(FVertexList.IndexOf(v));
+end;
+
+function TGSimpleSparseDGraph.RemoveVertexI(aIndex: SizeInt): Boolean;
+begin
+  Result := (aIndex >= 0) and (aIndex < FVertexList.Count);
+  if Result then
+    DoRemoveVertex(aIndex);
+end;
+
+function TGSimpleSparseDGraph.AddEdge(constref aSrc, aDst: TVertex; aData: TEdgeData): Boolean;
+var
+  SrcIdx, DstIdx: SizeInt;
+begin
+  AddVertex(aSrc, SrcIdx);
+  AddVertex(aDst, DstIdx);
+  Result := DoAddEdge(SrcIdx, DstIdx, aData);
+end;
+
+function TGSimpleSparseDGraph.AddEdge(constref aSrc, aDst: TVertex): Boolean;
+begin
+  Result := AddEdge(aSrc, aDst, CFData);
+end;
+
+function TGSimpleSparseDGraph.AddEdgeI(aSrc, aDst: SizeInt; aData: TEdgeData): Boolean;
+begin
+  if (aSrc < 0) or (aSrc >= FVertexList.Count) then
+    exit(False);
+  if (aDst < 0) or (aDst >= FVertexList.Count) then
+    exit(False);
+  Result := DoAddEdge(aSrc, aDst, aData);
+end;
+
+function TGSimpleSparseDGraph.AddEdgeI(aSrc, aDst: SizeInt): Boolean;
+begin
+  Result := AddEdgeI(aSrc, aDst, CFData);
+end;
+
+function TGSimpleSparseDGraph.RemoveEdge(constref aSrc, aDst: TVertex): Boolean;
+begin
+  Result := RemoveEdgeI(FVertexList.IndexOf(aSrc), FVertexList.IndexOf(aDst));
+end;
+
+function TGSimpleSparseDGraph.RemoveEdgeI(aSrc, aDst: SizeInt): Boolean;
+begin
+  if (aSrc < 0) or (aSrc >= FVertexList.Count) then
+    exit(False);
+  if (aDst < 0) or (aDst >= FVertexList.Count) then
+    exit(False);
+  Result := DoRemoveEdge(aSrc, aDst);
+end;
 
 end.
 
