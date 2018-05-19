9c5c40707db60481dca9dc0d2b3c71ed8f7fcbea
avk959 <rbyter@mail.ru>
2018-05-19 22:51:16 +0700

    added LGSimpleDGraph

---
+++ source/LGSimpleDGraph.pas
@@ -0,0 +1,54 @@
+{****************************************************************************
+*                                                                           *
+*   This file is part of the LGenerics package.                             *
+*   Generic simple directed graph implementation.                           *
+*                                                                           *
+*   Copyright(c) 2018 A.Koverdyaev(avk)                                     *
+*                                                                           *
+*   This code is free software; you can redistribute it and/or modify it    *
+*   under the terms of the Apache License, Version 2.0;                     *
+*   You may obtain a copy of the License at                                 *
+*     http://www.apache.org/licenses/LICENSE-2.0.                           *
+*                                                                           *
+*  Unless required by applicable law or agreed to in writing, software      *
+*  distributed under the License is distributed on an "AS IS" BASIS,        *
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+*  See the License for the specific language governing permissions and      *
+*  limitations under the License.                                           *
+*                                                                           *
+*****************************************************************************}
+unit LGSimpleDGraph;
+
+{$mode objfpc}{$H+}
+{$INLINE ON}
+{$MODESWITCH ADVANCEDRECORDS}
+{$MODESWITCH NESTEDPROCVARS}
+
+interface
+
+uses
+  Classes, SysUtils,
+  LGUtils,
+  {%H-}LGHelpers,
+  LGArrayHelpers,
+  LGStack,
+  LGQueue,
+  LGVector,
+  LGraphUtils,
+  LGraphType;
+
+type
+
+  { TGSimpleSparseDGraph simple sparse directed graph based on adjacency lists;
+      functor TVertexEqRel must provide:
+        class function HashCode([const[ref]] aValue: TVertex): SizeInt;
+        class function Equal([const[ref]] L, R: TVertex): Boolean; }
+  generic TGSimpleSparseDGraph<TVertex, TEdgeData, TVertexEqRel> = class(
+    specialize TGCustomSimpleSparseGraph<TVertex, TEdgeData, TVertexEqRel>)
+
+  end;
+
+implementation
+
+end.
+
