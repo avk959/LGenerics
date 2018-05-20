077cfacee4c05bd0bae1485a5eca27c77f8a1173
avk959 <rbyter@mail.ru>
2018-05-20 20:13:01 +0700

    added unit LGSimpleDGraph

--- packages/LGenerics.pas
+++ packages/LGenerics.pas
@@ -11,7 +11,7 @@ uses
   LGUtils, LGCustomContainer, LGHash, LGFunction, LGArrayHelpers, LGHelpers, LGMiscUtils, LGStack, LGStrConst, 
   LGPriorityQueue, LGDeque, LGHashSet, LGHashMultiSet, LGHashMap, LGTreeSet, LGTreeMultiSet, LGTreeMap, LGAvlTree, 
   LGStrHelpers, LGHashTable, LGVector, LGMultiMap, LGBiMap, LGAsync, LGQueue, LGTable2D, LGList, LGraphType, 
-  LGraphUtils, LGSimpleUGraph;
+  LGraphUtils, LGSimpleUGraph, LGSimpleDGraph;
 
 implementation
 
