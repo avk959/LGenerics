7cf8f8a9f5e54693ed06c6736909175528e8f266
avk959 <rbyter@mail.ru>
2018-12-12 13:49:40 +0700

    added some tests for LGSimpleGraph.pas

--- test/testset/LGSimpleGraphTest.pas
+++ test/testset/LGSimpleGraphTest.pas
@@ -19,11 +19,20 @@ type
   TSimpleGraphTest = class(TTestCase)
   private
   type
-    TGraph = TIntChart;
-    TRef   = specialize TGAutoRef<TGraph>;
+    TGraph  = TIntChart;
+    TRef    = specialize TGAutoRef<TGraph>;
+    THelper = specialize TGOrdinalArrayHelper<SizeInt>;
+
     function  GenerateTestGr1: TGraph;
+    function  GenerateTestGr2: TGraph;
   published
     procedure SaveToStream;
+    procedure Clone;
+    procedure Degree;
+    procedure Connected;
+    procedure SeparateCount1;
+    procedure SeparateCount2;
+    procedure SeparateGraph;
   end;
 
 implementation
@@ -36,6 +45,15 @@ begin
                    8, 7, 9, 10, 9, 11, 9, 12, 11, 12]);
 end;
 
+function TSimpleGraphTest.GenerateTestGr2: TGraph;
+begin
+  Result := TGraph.Create;
+  Result.AddVertexRange(1, 16);
+  Result.AddEdges([1, 2, 1, 3, 1, 4, 1, 6, 1, 7, 3, 4, 4, 6, 4, 5, 7, 4, 5, 10, 7, 10,
+                   10, 11, 10, 12, 10, 13, 12, 13,
+                   8, 9, 8, 14, 8, 15, 9, 15, 9, 16, 14, 15, 14, 16]);
+end;
+
 procedure TSimpleGraphTest.SaveToStream;
 var
   Ref, Ref2: TRef;
@@ -46,11 +64,11 @@ var
 begin
   Title := 'Test graph';
   Description := 'for testing purposes';
-  Ref.Instance := GenerateTestGr1;
+  {%H-}Ref.Instance := GenerateTestGr1;
   g := Ref;
-  g.Title := Title;
   AssertTrue(g.VertexCount = 13);
   AssertTrue(g.EdgeCount = 17);
+  g.Title := Title;
   g.Description.Text := Description;
   g2 := Ref2;
   Stream := TMemoryStream.Create;
@@ -71,6 +89,113 @@ begin
   AssertTrue(g2.Description.Text = Description);
 end;
 
+procedure TSimpleGraphTest.Clone;
+var
+  Ref, Ref2: TRef;
+  g, g2: TGraph;
+  e: TGraph.TEdge;
+  Title, Description: string;
+begin
+  Title := 'Test graph';
+  Description := 'for testing purposes';
+  {%H-}Ref.Instance := GenerateTestGr1;
+  g := Ref;
+  AssertTrue(g.VertexCount = 13);
+  AssertTrue(g.EdgeCount = 17);
+  g.Title := Title;
+  g.Description.Text := Description;
+  {%H-}Ref2.Instance := g.Clone;
+  g2 := Ref2;
+  AssertTrue(g2.VertexCount = 13);
+  AssertTrue(g2.EdgeCount = 17);
+  for e in g.Edges do
+    AssertTrue(g2.ContainsEdgeI(e.Source, e.Destination));
+  for e in g2.Edges do
+    AssertTrue(g.ContainsEdgeI(e.Source, e.Destination));
+  AssertTrue(g2.Title = Title);
+  AssertTrue(g2.Description.Text = Description);
+end;
+
+procedure TSimpleGraphTest.Degree;
+var
+  Ref: TRef;
+  g: TGraph;
+  Raised: Boolean = False;
+begin
+  g := Ref;
+  g.AddEdges([1, 2]);
+  AssertTrue(g.Degree(1) = 1);
+  AssertTrue(g.Degree(2) = 1);
+  g.RemoveVertex(2);
+  AssertTrue(g.Degree(1) = 0);
+  try
+    AssertTrue(g.Degree(2) = 0);
+  except
+    on e: EGraphError do
+      Raised := True;
+  end;
+  AssertTrue(Raised);
+  g.AddEdges([1, 2, 1, 3, 1, 4]);
+  AssertTrue(g.Degree(1) = 3);
+  AssertTrue(g.Degree(2) = 1);
+  AssertTrue(g.Degree(3) = 1);
+  AssertTrue(g.Degree(4) = 1);
+end;
+
+procedure TSimpleGraphTest.Connected;
+var
+  Ref: TRef;
+  g: TGraph;
+begin
+  g := {%H-}Ref;
+  AssertFalse(g.Connected);
+  g.AddVertex(1);
+  AssertTrue(g.Connected);
+  g.AddVertexRange(2, 4);
+  AssertFalse(g.Connected);
+  g.AddEdges([1, 2, 1, 3, 1, 4]);
+  AssertTrue(g.Connected);
+  g.RemoveEdge(1, 3);
+  AssertFalse(g.Connected);
+  g.RemoveVertex(3);
+  AssertTrue(g.Connected);
+end;
+
+procedure TSimpleGraphTest.SeparateCount1;
+var
+  Ref: TRef;
+  g: TGraph;
+begin
+  {%H-}Ref.Instance := GenerateTestGr1;
+  g := Ref;
+   AssertTrue(g.SeparateCount = 1);
+end;
+
+procedure TSimpleGraphTest.SeparateCount2;
+var
+  Ref: TRef;
+  g: TGraph;
+begin
+  {%H-}Ref.Instance := GenerateTestGr2;
+  g := Ref;
+   AssertTrue(g.SeparateCount = 2);
+end;
+
+procedure TSimpleGraphTest.SeparateGraph;
+var
+  Ref, Ref2: TRef;
+  g, g2: TGraph;
+begin
+  {%H-}Ref.Instance := GenerateTestGr2;
+  g := Ref;
+  AssertTrue(g.VertexCount = 16);
+  Ref2.Instance := g.SeparateGraph(8);
+  g2 := Ref2;
+  AssertTrue(g2.VertexCount = 5);
+  Ref2.Instance := g.SeparateGraph(1);
+  g2 := Ref2;
+  AssertTrue(g2.VertexCount = 11);
+end;
 
 
 initialization
