c1e66e67c6e88a703209697b9f61989309f4ed02
avk959 <rbyter@mail.ru>
2019-02-05 13:15:35 +0700

    fixed some lapses

--- lgenerics/LGenerics.pas
+++ lgenerics/LGenerics.pas
@@ -7,6 +7,12 @@ unit LGenerics;
 {$warn 5023 off : no warning about unused units}
 interface
 
+uses
+  LGAbstractContainer, LGArrayHelpers, LGAsync, LGAvlTree, LGBiMap, LGDeque, LGFunction, LGHash, LGHashMap, 
+  LGHashMultiSet, LGHashSet, LGHashTable, LGHelpers, LGMiscUtils, LGMultiMap, LGPriorityQueue, LGQueue, 
+  LGSimpleDigraph, LGSimpleGraph, LGSparseGraph, LGStack, LGStrConst, LGStrHelpers, LGTable2D, LGTreeMap, 
+  LGTreeMultiSet, LGTreeSet, LGUtils, LGVector;
+
 implementation
 
 end.
