d168c3e3826d31187dd7143c3b77c8825b5a2965
avk959 <rbyter@mail.ru>
2019-11-17 14:28:55 +0700

    added some tests for TUInt128

--- test/testset/LGInt128Test.pas
+++ test/testset/LGInt128Test.pas
@@ -24,7 +24,16 @@ type
     procedure BitLength;
     procedure ToStringTest;
     procedure ToHexString;
+    procedure TryParse;
+    procedure CompareValue;
+    procedure CompareDWord;
+    procedure AssignDWord;
+    procedure AssignString;
+    procedure AsString;
     procedure IncValue;
+    procedure DecValue;
+    procedure ShiftLeft;
+    procedure ShiftRight;
   end;
 
 implementation
@@ -215,6 +224,163 @@ begin
   AssertTrue(I.ToHexString(42, True) = '$0000000000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF');
 end;
 
+procedure TUInt128Test.TryParse;
+var
+  I: TUInt128;
+begin
+  AssertFalse(TUInt128.TryParse('', I));
+  AssertFalse(TUInt128.TryParse('0x', I));
+  AssertFalse(TUInt128.TryParse('x', I));
+  AssertFalse(TUInt128.TryParse('-', I));
+  AssertFalse(TUInt128.TryParse('0-', I));
+
+  AssertTrue(TUInt128.TryParse('0', I));
+  AssertTrue(I.IsZero);
+
+  AssertTrue(TUInt128.TryParse('110', I));
+  AssertTrue(I.ToString = '110');
+
+  AssertTrue(TUInt128.TryParse('x11f', I));
+  AssertTrue(I.ToHexString(1) = '11F');
+  AssertTrue(TUInt128.TryParse('0x11f', I));
+  AssertTrue(I.ToHexString(1) = '11F');
+  AssertTrue(TUInt128.TryParse('0X11f', I));
+  AssertTrue(I.ToHexString(1) = '11F');
+
+
+  AssertTrue(TUInt128.TryParse('18446744073709551615', I));
+  AssertTrue(I.ToString = '18446744073709551615');
+
+  AssertTrue(TUInt128.TryParse('$ffffffffffffffff', I));
+  AssertTrue(I.ToHexString(1) = 'FFFFFFFFFFFFFFFF');
+
+  AssertTrue(TUInt128.TryParse('340282366920938463463374607431768211455', I));
+  AssertTrue(I.ToString = '340282366920938463463374607431768211455');
+end;
+
+procedure TUInt128Test.CompareValue;
+var
+  I, J: TUInt128;
+begin
+  I := Default(TUInt128);
+  J := Default(TUInt128);
+  AssertTrue(I = J);
+  AssertTrue(I >= J);
+  AssertFalse(I > J);
+  AssertTrue(I <= J);
+  AssertFalse(I < J);
+
+  I := TUInt128.Encode(1, 0);
+  AssertFalse(I = J);
+  AssertTrue(I >= J);
+  AssertTrue(I > J);
+  AssertFalse(I <= J);
+  AssertFalse(I < J);
+
+  J := TUInt128.Encode(0, 1);
+  AssertFalse(I = J);
+  AssertFalse(I >= J);
+  AssertFalse(I > J);
+  AssertTrue(I <= J);
+  AssertTrue(I < J);
+
+  I := TUInt128.Encode(0, 1);
+  AssertTrue(I = J);
+  AssertTrue(I >= J);
+  AssertFalse(I > J);
+  AssertTrue(I <= J);
+  AssertFalse(I < J);
+end;
+
+procedure TUInt128Test.CompareDWord;
+var
+  I: TUInt128;
+  d: DWord;
+begin
+  I := Default(TUInt128);
+  d := 0;
+  AssertTrue(I = d);
+  AssertTrue(I >= d);
+  AssertFalse(I > d);
+  AssertTrue(I <= d);
+  AssertFalse(I < d);
+
+  I := TUInt128.Encode(1, 0);
+  AssertFalse(I = d);
+  AssertTrue(I >= d);
+  AssertTrue(I > d);
+  AssertFalse(I <= d);
+  AssertFalse(I < d);
+
+  d := d.MaxValue;
+  AssertFalse(I = d);
+  AssertFalse(I >= d);
+  AssertFalse(I > d);
+  AssertTrue(I <= d);
+  AssertTrue(I < d);
+
+  I := TUInt128.Encode(0, 1);
+  AssertFalse(I = d);
+  AssertTrue(I >= d);
+  AssertTrue(I > d);
+  AssertFalse(I <= d);
+  AssertFalse(I < d);
+end;
+
+procedure TUInt128Test.AssignDWord;
+var
+  I: TUInt128;
+  d: DWord;
+begin
+  d := 0;
+  I := d;
+  AssertTrue(I.IsZero);
+
+  d := 100001;
+  I := d;
+  AssertTrue(I.ToString = '100001');
+
+  d := DWord.MaxValue;
+  I := d;
+  AssertTrue(I.ToString = d.MaxValue.ToString);
+end;
+
+procedure TUInt128Test.AssignString;
+var
+  I: TUInt128;
+begin
+  I := '0';
+  AssertTrue(I.IsZero);
+
+  I := '10001';
+  AssertTrue(I.ToString = '10001');
+
+  I := '0X11f';
+  AssertTrue(I.ToHexString(1) = '11F');
+
+
+  I := '340282366920938463463374607431768211122';
+  AssertTrue(I.ToString = '340282366920938463463374607431768211122');
+end;
+
+procedure TUInt128Test.AsString;
+var
+  I: TUInt128;
+  s: string;
+begin
+  I := Default(TUInt128);
+  s := I;
+  AssertTrue(s = '0');
+
+  I := I.Encode(10001, 0);
+  s := I;
+  AssertTrue(s = '10001');
+
+  I := I.MaxValue;
+  s := I;
+  AssertTrue(s = '340282366920938463463374607431768211455');
+end;
+
 procedure TUInt128Test.IncValue;
 var
   I: TUInt128;
@@ -239,6 +405,61 @@ begin
   I := I.MaxValue;
   Inc(I);
   AssertTrue(I.IsZero);
+end;
+
+procedure TUInt128Test.DecValue;
+var
+  I: TUInt128;
+begin
+  I := Default(TUInt128);
+  Dec(I);
+  AssertTrue(I = I.MaxValue);
+
+  I := I.Encode(1, 0);
+  Dec(I);
+  AssertTrue(I.IsZero);
+
+  I := I.Encode(0, 1);
+  Dec(I);
+  AssertTrue(I.Hi = 0);
+  AssertTrue(I.Lo = High(QWord));
+end;
+
+procedure TUInt128Test.ShiftLeft;
+var
+  I, Expect: TUInt128;
+  J: Integer;
+begin
+  I := Default(TUInt128);
+  for J := 1 to Pred(BitSizeOf(I)) do
+    AssertTrue(I shl J = 0);
+  I := 1;
+  AssertTrue(I shl 0 = 1);
+  AssertTrue(I shl BitSizeOf(I) = 1);
+  Expect := 0;
+  for J := 0 to Pred(BitSizeOf(I)) do
+    begin
+      Expect.Bits[J] := True;
+      AssertTrue(I shl J = Expect);
+      Expect.Bits[J] := False;
+    end;
+end;
+
+procedure TUInt128Test.ShiftRight;
+var
+  I, Expect: TUInt128;
+  J: Integer;
+begin
+  I := TUInt128(1) shl 127;
+  AssertTrue(I shr 0 = I);
+  AssertTrue(I shr BitSizeOf(I) = I);
+  Expect := 0;
+  for J := 0 to Pred(BitSizeOf(I)) do
+    begin
+      Expect.Bits[Pred(BitSizeOf(I)) - J] := True;
+      AssertTrue(I shr J = Expect);
+      Expect.Bits[Pred(BitSizeOf(I)) - J] := False;
+    end;
 end;
 
 
