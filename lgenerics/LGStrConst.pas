23399374773e8876572032ccbbd127b4902e5c4d
avk959 <rbyter@mail.ru>
2020-01-17 16:28:25 +0700

    added a new constant

--- lgenerics/LGStrConst.pas
+++ lgenerics/LGStrConst.pas
@@ -44,7 +44,7 @@ resourcestring
   SECantAcceptNegCount     = 'Can not accept negative TEntry.Count value';
   SECantAcceptNegLen       = 'Can not accept negative length value';
   SEInternalDataInconsist  = 'Internal data inconsistency';
-  SEInvalidInputFmt        = 'Invalid input parameter "%s" value()';
+  SEInputShouldAtLeastFmt  = 'Parameter "%s" should be at least %d';
   SEValueAlreadyExist      = 'Value already exists';
   SEKeyAlreadyExist        = 'Key already exists';
   SEResultUnknownFatal     = 'Result is unknown due to fatal exception';
