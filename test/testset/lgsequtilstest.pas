a7b88ec27ba1adc8b44bf5d0da5df80ca938b3f2
avk959 <rbyter@mail.ru>
2022-02-21 18:54:10 +0700

    added some tests for LevDistanceMyersUtf8

--- test/testset/lgsequtilstest.pas
+++ test/testset/lgsequtilstest.pas
@@ -18,6 +18,12 @@ type
     procedure LevenshteinDist;
     procedure LevenshteinDistMbr;
     procedure LevenshteinDistMbrBounded;
+    procedure LevenshteinDistMyers;
+    procedure LevenshteinDistMyersDQ;
+    procedure LevenshteinDistMyersLong;
+    procedure LevenshteinDistMyersBounded;
+    procedure LevenshteinDistMyersDQBounded;
+    procedure LevenshteinDistMyersLongBounded;
     procedure LcsDistMyers;
     procedure LcsDistMyersBounded;
   end;
@@ -96,7 +102,17 @@ const
     #$ed#$ae#$80#$ed#$b0#$80,
     #$ed#$ae#$80#$ed#$bf#$bf,
     #$ed#$af#$bf#$ed#$b0#$80,
-    #$ed#$af#$bf#$ed#$bf#$bf
+    #$ed#$af#$bf#$ed#$bf#$bf,
+
+    'Унылая пора! Очей очарованье!'#$ed#$a0#$80,
+    'Sad time! Oh charm!'#$ed#$ad#$bf,
+    'Кызганыч вакыт! Эх, сөйкемлелек!'#$ed#$ae#$80,
+    'เวลาเศร้า! โอ้เสน่ห์! '#$ed#$af#$bf,
+    '悲しい時間！ おお魅力！'#$ed#$b0#$80,
+    'ದುಃಖದ ಸಮಯ! ಓ ಮೋಡಿ! '#$ed#$be#$80,
+    '悲傷的時刻！ 哦魅力！'#$e0#$80#$80,
+    'Kədərli vaxt! Ey cazibədarlıq!'#$e0#$80#$80,
+    'სამწუხარო დრო! ოჰ ხიბლი!'#$e0#$9f#$bf
   );
 
 procedure TTestUtf8Utils.ValidateDfa;
@@ -392,6 +408,286 @@ begin
   s2 := '一，在寒冷的冬天，我从森林里走来。';
   AssertTrue(LevDistanceMbrUtf8(s1, s2) = 2);
   AssertTrue(LevDistanceMbrUtf8(s2, s1, 1) = -1);
+end;
+
+procedure TTestUtf8Utils.LevenshteinDistMyers;
+var
+  s1, s2: string;
+begin
+  s1 := '';
+  s2 := 'привет';
+  AssertTrue(LevDistanceMyersUtf8(s1, s1) = 0);
+  AssertTrue(LevDistanceMyersUtf8(s2, s2) = 0);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 6);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 6);
+
+  s1 := 'аб';
+  s2 := 'аа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 1);
+
+  s1 := 'аб';
+  s2 := 'ба';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 2);
+
+  s1 := 'аб';
+  s2 := 'ааа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 2);
+
+  s1 := 'а';
+  s2 := 'ббб';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 3);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 3);
+
+  s1 := 'аабабаб';
+  s2 := 'аббаа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 3);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 3);
+
+  s1 := 'нелли';
+  s2 := 'елли';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 1);
+
+  s1 := 'нелли';
+  s2 := 'еллия';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 2);
+
+  s1 := 'мостик';
+  s2 := 'костик';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 1);
+
+  s1 := 'мостик';
+  s2 := 'костяка';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 3);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 3);
+
+  s1 := 'дистанция';
+  s2 := 'дисперсия';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 4);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 4);
+
+  s1 := 'левенштейн';
+  s2 := 'франкенштейн';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 5);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 5);
+
+  s1 := 'ааааааа';
+  s2 := 'ааааааа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 0);
+
+  s1 := 'ааааааа';
+  s2 := 'бббббббб';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 8);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 8);
+
+  s1 := 'аааббаааа';
+  s2 := 'ааааааа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 2);
+
+  s1 := 'а';
+  s2 := 'б';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 1);
+
+  s1 := '一天，在寒冷的冬天，我从森林里走出来。';
+  s2 := '一，在寒冷的冬天，我从森林里走来。';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1) = 2);
+end;
+
+procedure TTestUtf8Utils.LevenshteinDistMyersDQ;
+const
+  s1:  string = 'Ну, здравствуйте, здравствуйте. Je vois que je vous fais peur, садитесь и рассказывайте';
+  s2:  string = 'Dieu, quelle virulente sortie![4] – отвечал, нисколько не смутясь такою встречей';
+  s3:  string = 'Вы не видали еще, – или: – вы не знакомы с ma tante? – говорила Анна Павловна';
+  s4:  string = 'Il va se faire tuer. Dites-moi, pourquoi cette vilaine guerre, – сказала она князю Василию';
+  s5:  string = 'Да, я слышал про его план вечного мира, и это очень интересно, но едва ли возможно…';
+  s6:  string = 'шепнула Анна Павловна одному. – Le vicomte est un parfait conteur, – проговорила она другому';
+  s7:  string = 'Княжна облокотила свою открытую полную руку на столик и не нашла нужным что-либо сказать';
+  s8:  string = 'Le charmant Hippolyte поражал своим необыкновенным сходством с сестрою-красавицею';
+  s9:  string = 'Ce n’est pas une histoire de revenants? – сказал он, усевшись подле княгини и торопливо пристроив к глазам свой лорнет';
+  s10: string = 'сказал князь Ипполит таким тоном, что видно было, – он сказал эти слова, а потом уже понял, что они значили';
+  s11: string = 'Средство – европейское равновесие и droit des gens, – говорил аббат ';
+  s12: string = 'Образуйте мне этого медведя, – сказал он. – Вот он месяц живет у меня, и в первый раз я его вижу в свете';
+  s13: string = 'Пожилая дама носила имя княгини Друбецкой, одной из лучших фамилий России, но она была бедна';
+  s14: string = 'Но влияние в свете есть капитал, который надо беречь, чтоб он не исчез. Князь Василий знал это';
+begin
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = LevDistanceUtf8(s1, s2));
+  AssertTrue(LevDistanceMyersUtf8(s3, s4) = LevDistanceUtf8(s3, s4));
+  AssertTrue(LevDistanceMyersUtf8(s5, s6) = LevDistanceUtf8(s5, s6));
+  AssertTrue(LevDistanceMyersUtf8(s7, s8) = LevDistanceUtf8(s7, s8));
+  AssertTrue(LevDistanceMyersUtf8(s9, s10) = LevDistanceUtf8(s9, s10));
+  AssertTrue(LevDistanceMyersUtf8(s11, s12) = LevDistanceUtf8(s11, s12));
+  AssertTrue(LevDistanceMyersUtf8(s13, s14) = LevDistanceUtf8(s13, s14));
+end;
+
+procedure TTestUtf8Utils.LevenshteinDistMyersLong;
+const
+  s1:  string = 'Этого не обещаю. Вы знаете, как осаждают Кутузова с тех пор, как он назначен главнокомандующим. Он мне сам говорил, что все московские барыни сговорились отдать ему всех своих детей в адъютанты';
+  s2:  string = 'Нет, обещайте, обещайте, Basile, – сказала вслед ему Анна Михайловна, с улыбкой молодой кокетки, которая когда-то, должно быть, была ей свойственна, а теперь так не шла к ее истощенному лицу.';
+  s3:  string = 'Les souverains? Je ne parle pas de la Russie, – сказал виконт учтиво и безнадежно. – Les souverains, madame? Qu’ont ils fait pour Louis XVI, pour la reine, pour madame Elisabeth? Rien, – продолжал он, одушевляясь.';
+  s4:  string = 'И он, презрительно вздохнув, опять переменил положение. Князь Ипполит, долго смотревший в лорнет на виконта, вдруг при этих словах повернулся всем телом к маленькой княгине и, попросив у нее иголку, стал показывать ей, рисуя иголкой на столе, герб Конде.';
+  s5:  string = 'Ежели еще год Бонапарте останется на престоле Франции, – продолжал виконт начатый разговор, с видом человека, не слушающего других, но в деле, лучше всех ему известном, следящего только за ходом своих мыслей, – то дела пойдут слишком далеко.';
+  s6:  string = 'Не успели еще Анна Павловна и другие улыбкой оценить этих слов виконта, как Пьер опять ворвался в разговор, и Анна Павловна, хотя и предчувствовавшая, что он скажет что-нибудь неприличное, уже не могла остановить его.';
+  s7:  string = '– Свобода и равенство, – презрительно сказал виконт, как будто решившийся, наконец, серьезно доказать этому юноше всю глупость его речей, – всё громкие слова, которые уже давно компрометировались.';
+  s8:  string = '– Mais, mon cher monsieur Pierre, – сказала Анна Павловна, – как же вы объясняете великого человека, который мог казнить герцога, наконец просто человека, без суда и без вины?';
+  s9:  string = '– Как вы хотите, чтоб он всем отвечал вдруг? – сказал князь Андрей. – Притом надо в поступках государственного человека различать поступки частного лица, полководца или императора.';
+  s10: string = 'Тут князь Ипполит фыркнул и захохотал гораздо прежде своих слушателей, что произвело невыгодное для рассказчика впечатление. Однако многие, и в том числе пожилая дама и Анна Павловна, улыбнулись.';
+  s11: string = 'Пьер был неуклюж. Толстый, выше обыкновенного роста, широкий, с огромными красными руками, он, как говорится, не умел войти в салон и еще менее умел из него выйти, то есть перед выходом сказать что-нибудь особенно приятное.';
+  s12: string = 'Два лакея, один княгинин, другой его, дожидаясь, когда они кончат говорить, стояли с шалью и рединготом и слушали их, непонятный им, французский говор с такими лицами, как будто они понимали, что говорится, но не хотели показывать этого.';
+  s13: string = 'Князь Ипполит торопливо надел свой редингот, который у него, по-новому, был длиннее пяток, и, путаясь в нем, побежал на крыльцо за княгиней, которую лакей подсаживал в карету.';
+  s14: string = '– Ты не понимаешь, отчего я это говорю, – продолжал он. – Ведь это целая история жизни. Ты говоришь, Бонапарте и его карьера, – сказал он, хотя Пьер и не говорил про Бонапарте.';
+begin
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = LevDistanceUtf8(s1, s2));
+  AssertTrue(LevDistanceMyersUtf8(s3, s4) = LevDistanceUtf8(s3, s4));
+  AssertTrue(LevDistanceMyersUtf8(s5, s6) = LevDistanceUtf8(s5, s6));
+  AssertTrue(LevDistanceMyersUtf8(s7, s8) = LevDistanceUtf8(s7, s8));
+  AssertTrue(LevDistanceMyersUtf8(s9, s10) = LevDistanceUtf8(s9, s10));
+  AssertTrue(LevDistanceMyersUtf8(s11, s12) = LevDistanceUtf8(s11, s12));
+  AssertTrue(LevDistanceMyersUtf8(s13, s14) = LevDistanceUtf8(s13, s14));
+end;
+
+procedure TTestUtf8Utils.LevenshteinDistMyersBounded;
+var
+  s1, s2: string;
+begin
+  s1 := '';
+  s2 := 'привет';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 6);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1, 5) = -1);
+
+  s1 := 'аб';
+  s2 := 'аа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 0) = -1);
+
+  s1 := 'аб';
+  s2 := 'ба';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 1) = -1);
+
+  s1 := 'аб';
+  s2 := 'ааа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 1) = -1);
+
+  s1 := 'а';
+  s2 := 'ббб';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 3);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 2) = -1);
+
+  s1 := 'аабабаб';
+  s2 := 'аббаа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 3);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 2) = -1);
+
+  s1 := 'нелли';
+  s2 := 'елли';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 0) = -1);
+
+  s1 := 'нелли';
+  s2 := 'еллия';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 1) = -1);
+
+  s1 := 'мостик';
+  s2 := 'костик';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 0) = -1);
+
+  s1 := 'мостик';
+  s2 := 'костяка';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 3);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 2) = -1);
+
+  s1 := 'дистанция';
+  s2 := 'дисперсия';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 4);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 3) = -1);
+
+  s1 := 'левенштейн';
+  s2 := 'франкенштейн';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 5);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 4) = -1);
+
+  s1 := 'ааааааа';
+  s2 := 'бббббббб';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 8);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 7) = -1);
+
+  s1 := 'аааббаааа';
+  s2 := 'ааааааа';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 1) = -1);
+
+  s1 := 'а';
+  s2 := 'б';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 1);
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, 0) = -1);
+
+  s1 := '一天，在寒冷的冬天，我从森林里走出来。';
+  s2 := '一，在寒冷的冬天，我从森林里走来。';
+  AssertTrue(LevDistanceMyersUtf8(s1, s2) = 2);
+  AssertTrue(LevDistanceMyersUtf8(s2, s1, 1) = -1);
+end;
+
+procedure TTestUtf8Utils.LevenshteinDistMyersDQBounded;
+const
+  s1:  string = 'Ну, здравствуйте, здравствуйте. Je vois que je vous fais peur, садитесь и рассказывайте';
+  s2:  string = 'Dieu, quelle virulente sortie![4] – отвечал, нисколько не смутясь такою встречей';
+  s3:  string = 'Вы не видали еще, – или: – вы не знакомы с ma tante? – говорила Анна Павловна';
+  s4:  string = 'Il va se faire tuer. Dites-moi, pourquoi cette vilaine guerre, – сказала она князю Василию';
+  s5:  string = 'Да, я слышал про его план вечного мира, и это очень интересно, но едва ли возможно…';
+  s6:  string = 'шепнула Анна Павловна одному. – Le vicomte est un parfait conteur, – проговорила она другому';
+  s7:  string = 'Княжна облокотила свою открытую полную руку на столик и не нашла нужным что-либо сказать';
+  s8:  string = 'Le charmant Hippolyte поражал своим необыкновенным сходством с сестрою-красавицею';
+  s9:  string = 'Ce n’est pas une histoire de revenants? – сказал он, усевшись подле княгини и торопливо пристроив к глазам свой лорнет';
+  s10: string = 'сказал князь Ипполит таким тоном, что видно было, – он сказал эти слова, а потом уже понял, что они значили';
+  s11: string = 'Средство – европейское равновесие и droit des gens, – говорил аббат ';
+  s12: string = 'Образуйте мне этого медведя, – сказал он. – Вот он месяц живет у меня, и в первый раз я его вижу в свете';
+  s13: string = 'Пожилая дама носила имя княгини Друбецкой, одной из лучших фамилий России, но она была бедна';
+  s14: string = 'Но влияние в свете есть капитал, который надо беречь, чтоб он не исчез. Князь Василий знал это';
+begin
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, Pred(LevDistanceUtf8(s1, s2))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s3, s4, Pred(LevDistanceUtf8(s3, s4))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s5, s6, Pred(LevDistanceUtf8(s5, s6))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s7, s8, Pred(LevDistanceUtf8(s7, s8))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s9, s10, Pred(LevDistanceUtf8(s9, s10))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s11, s12, Pred(LevDistanceUtf8(s11, s12))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s13, s14, Pred(LevDistanceUtf8(s13, s14))) = -1);
+end;
+
+procedure TTestUtf8Utils.LevenshteinDistMyersLongBounded;
+const
+  s1:  string = 'Этого не обещаю. Вы знаете, как осаждают Кутузова с тех пор, как он назначен главнокомандующим. Он мне сам говорил, что все московские барыни сговорились отдать ему всех своих детей в адъютанты';
+  s2:  string = 'Нет, обещайте, обещайте, Basile, – сказала вслед ему Анна Михайловна, с улыбкой молодой кокетки, которая когда-то, должно быть, была ей свойственна, а теперь так не шла к ее истощенному лицу.';
+  s3:  string = 'Les souverains? Je ne parle pas de la Russie, – сказал виконт учтиво и безнадежно. – Les souverains, madame? Qu’ont ils fait pour Louis XVI, pour la reine, pour madame Elisabeth? Rien, – продолжал он, одушевляясь.';
+  s4:  string = 'И он, презрительно вздохнув, опять переменил положение. Князь Ипполит, долго смотревший в лорнет на виконта, вдруг при этих словах повернулся всем телом к маленькой княгине и, попросив у нее иголку, стал показывать ей, рисуя иголкой на столе, герб Конде.';
+  s5:  string = 'Ежели еще год Бонапарте останется на престоле Франции, – продолжал виконт начатый разговор, с видом человека, не слушающего других, но в деле, лучше всех ему известном, следящего только за ходом своих мыслей, – то дела пойдут слишком далеко.';
+  s6:  string = 'Не успели еще Анна Павловна и другие улыбкой оценить этих слов виконта, как Пьер опять ворвался в разговор, и Анна Павловна, хотя и предчувствовавшая, что он скажет что-нибудь неприличное, уже не могла остановить его.';
+  s7:  string = '– Свобода и равенство, – презрительно сказал виконт, как будто решившийся, наконец, серьезно доказать этому юноше всю глупость его речей, – всё громкие слова, которые уже давно компрометировались.';
+  s8:  string = '– Mais, mon cher monsieur Pierre, – сказала Анна Павловна, – как же вы объясняете великого человека, который мог казнить герцога, наконец просто человека, без суда и без вины?';
+  s9:  string = '– Как вы хотите, чтоб он всем отвечал вдруг? – сказал князь Андрей. – Притом надо в поступках государственного человека различать поступки частного лица, полководца или императора.';
+  s10: string = 'Тут князь Ипполит фыркнул и захохотал гораздо прежде своих слушателей, что произвело невыгодное для рассказчика впечатление. Однако многие, и в том числе пожилая дама и Анна Павловна, улыбнулись.';
+  s11: string = 'Пьер был неуклюж. Толстый, выше обыкновенного роста, широкий, с огромными красными руками, он, как говорится, не умел войти в салон и еще менее умел из него выйти, то есть перед выходом сказать что-нибудь особенно приятное.';
+  s12: string = 'Два лакея, один княгинин, другой его, дожидаясь, когда они кончат говорить, стояли с шалью и рединготом и слушали их, непонятный им, французский говор с такими лицами, как будто они понимали, что говорится, но не хотели показывать этого.';
+  s13: string = 'Князь Ипполит торопливо надел свой редингот, который у него, по-новому, был длиннее пяток, и, путаясь в нем, побежал на крыльцо за княгиней, которую лакей подсаживал в карету.';
+  s14: string = '– Ты не понимаешь, отчего я это говорю, – продолжал он. – Ведь это целая история жизни. Ты говоришь, Бонапарте и его карьера, – сказал он, хотя Пьер и не говорил про Бонапарте.';
+begin
+  AssertTrue(LevDistanceMyersUtf8(s1, s2, Pred(LevDistanceUtf8(s1, s2))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s3, s4, Pred(LevDistanceUtf8(s3, s4))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s5, s6, Pred(LevDistanceUtf8(s5, s6))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s7, s8, Pred(LevDistanceUtf8(s7, s8))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s9, s10, Pred(LevDistanceUtf8(s9, s10))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s11, s12, Pred(LevDistanceUtf8(s11, s12))) = -1);
+  AssertTrue(LevDistanceMyersUtf8(s13, s14, Pred(LevDistanceUtf8(s13, s14))) = -1);
 end;
 
 procedure TTestUtf8Utils.LcsDistMyers;
