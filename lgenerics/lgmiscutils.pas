33cd336d8f9c59bce2922349b3f01b3c71379736
avk959 <rbyter@mail.ru>
2022-04-20 13:05:56 +0700

    added useful functions

--- lgenerics/lgmiscutils.pas
+++ lgenerics/lgmiscutils.pas
@@ -29,12 +29,13 @@ interface
 
 uses
 
-  Classes, SysUtils, math,
+  Classes, SysUtils, math, FpJson,
   lgUtils,
   {%H-}lgHelpers,
   lgArrayHelpers,
   lgAbstractContainer,
   lgHashMap,
+  lgJson,
   lgStrConst;
 
 type
@@ -430,6 +431,9 @@ type
 
   function CmdLineOptions: TCmdLineOptions;
 
+  function JsonNode2Data(aNode: TJsonNode): TJsonData;
+  function JsonData2Node(aData: TJsonData): TJsonNode;
+
 implementation
 {$B-}{$COPERATORS ON}
 
@@ -581,6 +585,74 @@ begin
   if Options = nil then
     ParseOptions;
   Result := System.Copy(Options, 0, System.Length(Options)) ;
+end;
+
+{$PUSH}{$WARN 4046 OFF}
+function JsonNode2Data(aNode: TJsonNode): TJsonData;
+  function CopyNode(aNode: TJsonNode): TJsonData;
+  var
+    I: SizeInt;
+  begin
+    case aNode.Kind of
+      jvkUnknown: Result := TJsonData.Create;
+      jvkNull:    Result := CreateJson;
+      jvkFalse:   Result := CreateJson(False);
+      jvkTrue:    Result := CreateJson(True);
+      jvkNumber:  Result := CreateJson(aNode.AsNumber);
+      jvkString:  Result := CreateJson(aNode.AsString);
+      jvkArray:
+        begin
+          Result := TJsonArray.Create;
+          for I := 0 to Pred(aNode.Count) do
+            TJsonArray(Result).Add(CopyNode(aNode.Items[I]));
+        end;
+      jvkObject:
+        begin
+          Result := TJsonObject.Create;
+          for I := 0 to Pred(aNode.Count) do
+            with aNode.Pairs[I] do
+              TJsonObject(Result).Add(Key, CopyNode(Value));
+        end;
+    end;
+  end;
+begin
+  if aNode = nil then
+    exit(nil);
+  Result := CopyNode(aNode);
+end;
+{$POP}
+
+function JsonData2Node(aData: TJsonData): TJsonNode;
+  procedure CopyData(aSrc: TJsonData; aDst: TJsonNode);
+  var
+    I: SizeInt;
+    e: TJsonEnum;
+  begin
+    case aSrc.JSONType of
+      jtUnknown: ;
+      jtNull:    aDst.AsNull;
+      jtBoolean: aDst.AsBoolean := aSrc.AsBoolean;
+      jtNumber:  aDst.AsNumber := aSrc.AsFloat;
+      jtString:  aDst.AsString := aSrc.AsString;
+      jtArray:
+        begin
+          aDst.AsArray;
+          for I := 0 to Pred(aSrc.Count) do
+            CopyData(aSrc.Items[I], aDst.AddNode);
+        end;
+      jtObject:
+        begin
+          aDst.AsObject;
+          for e in aSrc do
+            CopyData(e.Value, aDst.AddNode(e.Key));
+        end;
+    end;
+  end;
+begin
+  if aData = nil then
+    exit(nil);
+  Result := TJsonNode.Create;
+  CopyData(aData, Result);
 end;
 
 { TGTimSortAnc.TTimSortBase }
