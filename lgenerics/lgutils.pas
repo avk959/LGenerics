4b16c5261a44ba00003f54b3519bfad8268033ae
avk959 <rbyter@mail.ru>
2023-12-24 12:48:49 +0700

    added another exception class

--- lgenerics/lgutils.pas
+++ lgenerics/lgutils.pas
@@ -3,7 +3,7 @@
 *   This file is part of the LGenerics package.                             *
 *   Most common types and utils.                                            *
 *                                                                           *
-*   Copyright(c) 2018-2022 A.Koverdyaev(avk)                                *
+*   Copyright(c) 2018-2023 A.Koverdyaev(avk)                                *
 *                                                                           *
 *   This code is free software; you can redistribute it and/or modify it    *
 *   under the terms of the Apache License, Version 2.0;                     *
@@ -71,6 +71,7 @@ type
   ELGUpdateLock         = class(Exception);
   ELGListError          = class(Exception);
   ELGMapError           = class(Exception);
+  ELGObjectMapError     = class(ELGMapError);
   ELGTableError         = class(Exception);
 
 const
