af176fc14f240612b2f0b3a680b2d32b9dc19d1f
avk959 <rbyter@mail.ru>
2025-04-16 15:49:51 +0700

    added useful methods to TGDoubleHelper

--- lgenerics/lghelpers.pas
+++ lgenerics/lghelpers.pas
@@ -144,11 +144,10 @@ type
     class function Less(L, R: Pointer): Boolean; static; inline;
   end;
 
- // currently special float types (nan, infinity) are ignored
-
   TGSingleHelper = type helper(TSingleHelper) for Single
   private
   const
+    SIGN_FLAG     = QWord($80000000);
     MAX_EXACT_INT = Single($ffffff); //2^24 - 1
   public
     class function IsZero(const aValue: Single): Boolean; inline; static;
@@ -165,17 +164,69 @@ type
   TGDoubleHelper = type helper(TDoubleHelper) for Double
   private
   const
-    MAX_EXACT_INT  = Double($1fffffffffffff); //2^53 - 1
+    SIGN_FLAG  = QWord($8000000000000000);
+    EXP_MASK   = QWord($7ff0000000000000);
+    QUIET_FLAG = QWord($0008000000000000);
   public
-    class function IsZero(const aValue: Double): Boolean; inline; static;
-    class function HashCode(const aValue: Double): SizeInt; inline; static;
-    class function Equal(const L, R: Double): Boolean; inline; static;
-    class function Less(const L, R: Double): Boolean; inline; static;
-    class function IsExactInt(const aValue: Double): Boolean; inline; static;
-    class function IsExactInt(const aValue: Double; out aIntValue: Int64): Boolean; inline; static;
-    function IsZero: Boolean; inline;
-    function IsExactInt: Boolean; inline;
-    function IsExactInt(out aValue: Int64): Boolean; inline;
+  const
+    MAX_PAYLOAD   = QWord($7ffffffffffff);
+    MAX_EXACT_INT = Double($1fffffffffffff); //2^53 - 1
+  public
+    class function  IsZero(const aValue: Double): Boolean; inline; static;
+    class function  HashCode(const aValue: Double): SizeInt; inline; static;
+    class function  Equal(const L, R: Double): Boolean; inline; static;
+    class function  Less(const L, R: Double): Boolean; inline; static;
+    class function  IsExactInt(const aValue: Double): Boolean; inline; static;
+    class function  IsExactInt(const aValue: Double; out aIntValue: Int64): Boolean; inline; static;
+    class procedure Negate(var aValue: Double); inline; static;
+  { returns value with the magnitude of aMagn and the sign of aSgn }
+    class function  CopySign(const aMagn, aSgn: Double): Double; inline; static;
+  { returns True and in the aQuiet parameter whether it is quiet if aValue is NaN,
+    otherwise returns False }
+    class function  IsNan(const aValue: Double; out aQuiet: Boolean): Boolean; overload; inline; static;
+  { if aValue is a non-negative integer not exceeding MAX_PAYLOAD, returns a quiet NaN
+    with that payload; otherwize returns 0.0 }
+    class function  SetPayload(const aValue: Double): Double; static;
+  { if aValue is a non-negative integer not exceeding MAX_PAYLOAD, returns a signaling NaN
+    with that payload; otherwize returns 0.0 }
+    class function  SetSignalPayload(const aValue: Double): Double; static;
+  { if aValue is NaN then returns its payload, otherwise returns -1 }
+    class function  GetPayload(const aValue: Double): Double; inline; static;
+  { returns True if L and R are unordered, that is, at least one of them is NaN }
+    class function  Unordered(const L, R: Double): Boolean; inline; static;
+  { returns True if L > R; returns False if L <= R or unordered }
+    class function  IsGreater(const L, R: Double): Boolean; inline; static;
+  { returns True if L >= R; returns False if L < R or unordered }
+    class function  IsGreaterEqual(const L, R: Double): Boolean; inline; static;
+  { returns True if L < R; returns False if L >= R or unordered }
+    class function  IsLess(const L, R: Double): Boolean; inline; static;
+  { returns True if L <= R; returns False if L > R or unordered }
+    class function  IsLessEqual(const L, R: Double): Boolean; inline; static;
+  { returns True if L = R; returns False if L <> R or unordered }
+    class function  IsEqual(const L, R: Double): Boolean; inline; static;
+  { returns True if L <> R; returns False if L = R or unordered }
+    class function  IsNotEqual(const L, R: Double): Boolean; inline; static;
+  { TotalOrder predicate }
+    class function  TotalOrder(const L, R: Double): Boolean; static;
+  { returns quiet NaN if L and R unordered, otherwise
+    returns L if L <= R or R if R < L (-0 < +0) }
+    class function  Minimum(const L, R: Double): Double; static;
+  { returns quiet NaN if L and R unordered, otherwise
+    returns L if L >= R or R if R > L (+0 > -0) }
+    class function  Maximum(const L, R: Double): Double; static;
+  { returns the next representable value of aFrom in the direction of aDirectTo }
+    class function  NextAfter(const aFrom, aDirectTo: Double): Double; static;
+  { returns the value of a unit in the last position of aNum if aNum is finite number,
+    otherwise returns quiet NaN }
+    class function  Quantum(const aNum: Double): Double; static;
+    function  IsZero: Boolean; inline;
+    function  IsExactInt: Boolean; inline;
+    function  IsExactInt(out aValue: Int64): Boolean; inline;
+    procedure Negate; inline;
+    function  CopySign(const aSgn: Double): Double; inline;
+    function  IsNan(out aQuit: Boolean): Boolean; overload; inline;
+    function  NextToward(const aValue: Double): Double; inline;
+    function  Ulp: Double; inline;
   end;
 
 {$ifdef FPC_HAS_TYPE_EXTENDED}
@@ -194,7 +245,6 @@ type
   const
     MIN_EXACT_INT = Extended(Low(Int64));
     MAX_EXACT_INT = Extended(High(Int64));
-    EXP_MASK      = Word($7fff);
     SIGN_FLAG     = Word($8000);
   public
     class function IsZero(const aValue: Extended): Boolean; inline; static;
@@ -700,7 +750,7 @@ end;
 
 class function TGSingleHelper.IsZero(const aValue: Single): Boolean;
 begin
-  Result:= DWord(aValue) and DWord($7fffffff) = 0;
+  Result:= DWord(aValue) and not SIGN_FLAG = 0;
 end;
 
 class function TGSingleHelper.HashCode(const aValue: Single): SizeInt;
@@ -749,7 +799,7 @@ end;
 
 class function TGDoubleHelper.IsZero(const aValue: Double): Boolean;
 begin
-  Result := QWord(aValue) and QWord($7fffffffffffffff) = 0;
+  Result := QWord(aValue) and not SIGN_FLAG = 0;
 end;
 
 class function TGDoubleHelper.HashCode(const aValue: Double): SizeInt;
@@ -781,6 +831,204 @@ begin
   if Result then aIntValue := System.Trunc(aValue);
 end;
 
+class procedure TGDoubleHelper.Negate(var aValue: Double);
+begin
+  QWord(aValue) := QWord(aValue) xor SIGN_FLAG;
+end;
+
+class function TGDoubleHelper.CopySign(const aMagn, aSgn: Double): Double;
+begin
+  QWord(Result) := (QWord(aMagn) and not SIGN_FLAG) or (QWord(aSgn) and SIGN_FLAG)
+end;
+
+class function TGDoubleHelper.IsNan(const aValue: Double; out aQuiet: Boolean): Boolean;
+begin
+  Result := (QWord(aValue) and not SIGN_FLAG) > EXP_MASK;
+  if Result then aQuiet := QWord(aValue) and QUIET_FLAG <> 0;
+end;
+
+{$PUSH}{$WARN 5036 OFF : Local variable "$1" does not seem to be initialized }
+class function TGDoubleHelper.SetPayload(const aValue: Double): Double;
+var
+  I: Int64;
+begin
+  if IsExactInt(aValue, I) and (QWord(I) <= MAX_PAYLOAD) then
+    QWord(Result) := EXP_MASK or QUIET_FLAG or QWord(I)
+  else
+    Result := 0;
+end;
+
+class function TGDoubleHelper.SetSignalPayload(const aValue: Double): Double;
+var
+  I: Int64;
+begin
+  if IsExactInt(aValue, I) and(QWord(I) <= MAX_PAYLOAD) then
+    QWord(Result) := EXP_MASK or QWord(I)
+  else
+    Result := 0;
+end;
+{$POP}
+
+class function TGDoubleHelper.GetPayload(const aValue: Double): Double;
+begin
+  if (QWord(aValue) and not SIGN_FLAG) > EXP_MASK then
+    Result := Int64(QWord(aValue) and MAX_PAYLOAD)
+  else
+    Result := Double(-1);
+end;
+
+class function TGDoubleHelper.Unordered(const L, R: Double): Boolean;
+begin
+  Result := ((QWord(L) and not SIGN_FLAG) > EXP_MASK)or((QWord(R) and not SIGN_FLAG) > EXP_MASK);
+end;
+
+class function TGDoubleHelper.IsGreater(const L, R: Double): Boolean;
+begin
+  if Unordered(L, R) then exit(False);
+  Result := L > R;
+end;
+
+class function TGDoubleHelper.IsGreaterEqual(const L, R: Double): Boolean;
+begin
+  if Unordered(L, R) then exit(False);
+  Result := L >= R;
+end;
+
+class function TGDoubleHelper.IsLess(const L, R: Double): Boolean;
+begin
+  if Unordered(L, R) then exit(False);
+  Result := L < R;
+end;
+
+class function TGDoubleHelper.IsLessEqual(const L, R: Double): Boolean;
+begin
+  if Unordered(L, R) then exit(False);
+  Result := L <= R;
+end;
+
+class function TGDoubleHelper.IsEqual(const L, R: Double): Boolean;
+begin
+  if Unordered(L, R) then exit(False);
+  Result := L = R;
+end;
+
+class function TGDoubleHelper.IsNotEqual(const L, R: Double): Boolean;
+begin
+  if Unordered(L, R) then exit(False);
+  Result := L <> R;
+end;
+
+class function TGDoubleHelper.TotalOrder(const L, R: Double): Boolean;
+var
+  LNeg, RNeg: Boolean;
+begin
+  LNeg := QWord(L) and SIGN_FLAG <> 0;
+  RNeg := QWord(R) and SIGN_FLAG <> 0;
+  if LNeg xor RNeg then exit(LNeg); //////////////////
+  if LNeg then
+    Result := (QWord(L) and not SIGN_FLAG) > (QWord(R) and not SIGN_FLAG)
+  else
+    Result := (QWord(L) and not SIGN_FLAG) < (QWord(R) and not SIGN_FLAG)
+end;
+
+class function TGDoubleHelper.Minimum(const L, R: Double): Double;
+var
+  Quiet: Boolean;
+begin
+  if IsNan(L, Quiet) then
+    if Quiet then
+      exit(L)
+    else
+      exit(NaN)
+  else
+    if IsNan(R, Quiet) then
+      if Quiet then
+        exit(R)
+      else
+        exit(NaN);
+  if L < R then
+    Result := L
+  else
+    if R < L then
+      Result := R
+    else
+      if IsZero(L) and (L.Sign xor R.Sign) then
+        if L.Sign then
+          Result := L
+        else
+          Result := R
+      else
+        Result := L;
+end;
+
+class function TGDoubleHelper.Maximum(const L, R: Double): Double;
+var
+  Quiet: Boolean;
+begin
+  if IsNan(L, Quiet) then
+    if Quiet then
+      exit(L)
+    else
+      exit(NaN)
+  else
+    if IsNan(R, Quiet) then
+      if Quiet then
+        exit(R)
+      else
+        exit(NaN);
+  if L > R then
+    Result := L
+  else
+    if R > L then
+      Result := R
+    else
+      if IsZero(L) and (L.Sign xor R.Sign) then
+        if R.Sign then
+          Result := L
+        else
+          Result := R
+      else
+        Result := L;
+end;
+
+class function TGDoubleHelper.NextAfter(const aFrom, aDirectTo: Double): Double;
+var
+  v, vSign: QWord;
+begin
+  v := QWord(aFrom) and not SIGN_FLAG;
+  if v > EXP_MASK then
+    exit(aFrom)
+  else
+    if aDirectTo.IsNan then
+      exit(aDirectTo);
+  if aFrom = aDirectTo then exit(aFrom);
+  if v = 0 then
+    QWord(Result) := QWord(1) or QWord(aDirectTo) and SIGN_FLAG
+  else
+    begin
+      vSign := QWord(aFrom) and SIGN_FLAG;
+      if((vSign <> 0)xor aDirectTo.Sign)or((QWord(aDirectTo)and not SIGN_FLAG) < v)then
+        //to zero
+        QWord(Result) := Pred(v)and not SIGN_FLAG or vSign
+      else
+        QWord(Result) := Succ(v)and not SIGN_FLAG or vSign;
+    end;
+end;
+
+class function TGDoubleHelper.Quantum(const aNum: Double): Double;
+var
+  v: QWord;
+  vExp: Int32;
+begin
+  v := QWord(aNum) and not SIGN_FLAG;
+  if v >= EXP_MASK then exit(NaN);
+  vExp := Int32((QWord(aNum) shr 52) and $7ff) - 52;
+  if vExp <= 0 then
+    QWord(Result) := 1
+  else
+    QWord(Result) := QWord(vExp) shl 52;
+end;
+
 function TGDoubleHelper.IsZero: Boolean;
 begin
   Result := IsZero(Self);
@@ -796,12 +1044,35 @@ begin
   Result := IsExactInt(Self, aValue);
 end;
 
+procedure TGDoubleHelper.Negate;
+begin
+  Negate(Self);
+end;
+
+function TGDoubleHelper.CopySign(const aSgn: Double): Double;
+begin
+  Result := CopySign(Self, aSgn);
+end;
+
+function TGDoubleHelper.IsNan(out aQuit: Boolean): Boolean;
+begin
+  Result := IsNan(Self, aQuit);
+end;
+
+function TGDoubleHelper.NextToward(const aValue: Double): Double;
+begin
+  Result := NextAfter(Self, aValue);
+end;
+
+function TGDoubleHelper.Ulp: Double;
+begin
+  Result := Quantum(Self);
+end;
+
 {$ifdef FPC_HAS_TYPE_EXTENDED}
 class function TGExtendedHelper.IsZero(const aValue: Extended): Boolean;
-var
-  r: TRec80 absolute aValue;
 begin
-  Result := (r.PExp and EXP_MASK = 0) and (r.Mantis = 0);
+  Result := (TRec80(aValue).PExp and not SIGN_FLAG = 0)and(TRec80(aValue).Mantis = 0);
 end;
 
 class function TGExtendedHelper.HashCode(const aValue: Extended): SizeInt;
