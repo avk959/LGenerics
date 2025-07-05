a0513e8f32d21501f7b1cb835b4ff20cc69fd53b
avk959 <rbyter@mail.ru>
2025-07-05 14:04:12 +0700

    added incremental versions of some hash functions

--- lgenerics/lghash.pas
+++ lgenerics/lghash.pas
@@ -3,7 +3,7 @@
 *   This file is part of the LGenerics package.                             *
 *   Non-cryptographic hash functions for hash table lookup.                 *
 *                                                                           *
-*   Copyright(c) 2018-2024 A.Koverdyaev(avk)                                *
+*   Copyright(c) 2018-2025 A.Koverdyaev(avk)                                *
 *                                                                           *
 *   This code is free software; you can redistribute it and/or modify it    *
 *   under the terms of the Apache License, Version 2.0;                     *
@@ -44,6 +44,21 @@ type
     class function HashGuid(const aValue: TGuid; aSeed: DWord = 0): DWord; static;
   end;
 
+  { TIncXxHash32LE: incremental version of TxxHash32LE }
+  TIncXxHash32LE = record
+  private
+    FV,
+    FBuffer: array[0..3] of DWord;
+    FSeed,
+    FTotalBytes,
+    FHash: DWord;
+    FBufIndex: Integer;
+  public
+    procedure Reset(aSeed: DWord = 0);
+    procedure Add(const aBuffer; aCount: SizeInt);
+    function  Hash: DWord;
+  end;
+
   { TxxHash64LE: little endian implementation of Yann Collet's xxHash64 }
   TxxHash64LE = record
 {$IFDEF FPC_REQUIRES_PROPER_ALIGNMENT}
@@ -57,6 +72,21 @@ type
     class function HashDWord(aValue: DWord; aSeed: QWord = 0): QWord; static;
     class function HashQWord(aValue: QWord; aSeed: QWord = 0): QWord; static;
     class function HashGuid(const aValue: TGuid; aSeed: QWord = 0): QWord; static;
+  end;
+
+  { TIncXxHash64LE: incremental version of TxxHash64LE }
+  TIncXxHash64LE = record
+  private
+    FV,
+    FBuffer: array[0..3] of QWord;
+    FSeed,
+    FTotalBytes,
+    FHash: QWord;
+    FBufIndex: Integer;
+  public
+    procedure Reset(aSeed: QWord = 0);
+    procedure Add(const aBuffer; aCount: SizeInt);
+    function  Hash: QWord;
   end;
 
   TMurmur = class
@@ -117,6 +147,19 @@ type
     class function HashGuid(const aValue: TGuid; aSeed: DWord = 0): DWord; static;
   end;
 
+  { TIncMurmur3LE: incremental version of TMurmur3LE}
+  TIncMurmur3LE = record
+  private
+    FBuffer: array[0..3] of DWord;
+    FTotalBytes,
+    FHash: DWord;
+    FBufIndex: Integer;
+  public
+    procedure Reset(aSeed: DWord = 0);
+    procedure Add(const aBuffer; aCount: SizeInt);
+    function  Hash: DWord;
+  end;
+
   { TMurmur64aLE: little endian implementation of Austin Appleby's MurmurHash64A }
   TMurmur64aLE = class(TMurmur)
 {$IFDEF FPC_REQUIRES_PROPER_ALIGNMENT}
@@ -131,6 +174,7 @@ type
     class function HashQWord(aValue: QWord; aSeed: QWord = 0): QWord; static;
     class function HashGuid(const aValue: TGuid; aSeed: QWord = 0): QWord; static;
   end;
+
 
   function JdkHashW(aValue: Word): Word; inline;
   function JdkHash(aValue: DWord): DWord; inline;
@@ -439,6 +483,108 @@ begin
   Result := (Result xor Result shr 13) * c3;
   Result :=  Result xor Result shr 16;
 end;
+
+function IntMin(L: Integer; R: SizeInt): Integer; inline;
+begin
+  if L <= R then
+    Result := L
+  else
+    Result := Integer(R);
+end;
+
+{ TIncXxHash32LE }
+
+procedure TIncXxHash32LE.Reset(aSeed: DWord);
+begin
+  FV[0] := aSeed + c1 + c2;
+  FV[1] := aSeed + c2;
+  FV[2] := aSeed;
+  FV[3] := aSeed - c1;
+  FSeed := aSeed;
+  FTotalBytes := 0;
+  FHash := 0;
+  FBufIndex := 0;
+end;
+
+procedure TIncXxHash32LE.Add(const aBuffer; aCount: SizeInt);
+var
+  p: PByte;
+  ByteCount: Integer;
+begin
+  if aCount < 1 then exit;
+  Inc(FTotalBytes, aCount);
+  p := @aBuffer;
+  repeat
+    ByteCount := IntMin(SizeOf(FBuffer)-FBufIndex, aCount);
+    System.Move(p^, PByte(@FBuffer)[FBufIndex], ByteCount);
+    Dec(aCount, ByteCount);
+    Inc(FBufIndex, ByteCount);
+    Inc(p, ByteCount);
+    if FBufIndex = SizeOf(FBuffer) then
+      begin
+        FV[0] := RolDWord(FV[0] + FBuffer[0] * c2, 13) * c1;
+        FV[1] := RolDWord(FV[1] + FBuffer[1] * c2, 13) * c1;
+        FV[2] := RolDWord(FV[2] + FBuffer[2] * c2, 13) * c1;
+        FV[3] := RolDWord(FV[3] + FBuffer[3] * c2, 13) * c1;
+        FBufIndex := 0;
+      end;
+  until aCount = 0;
+end;
+
+function TIncXxHash32LE.Hash: DWord;
+var
+  p: PByte;
+  h: DWord;
+  ByteCount: Integer;
+begin
+  p := @FBuffer;
+  ByteCount := FBufIndex;
+  if FTotalBytes < SizeOf(FBuffer) then
+    h := FTotalBytes + FSeed + c5
+  else
+    h := RolDWord(FV[0], 1) + RolDWord(FV[1], 7) + RolDWord(FV[2], 12) + RolDWord(FV[3], 18) + FTotalBytes;
+  case ByteCount shr 2 of
+    1:
+      begin
+        h := RolDWord(h + FBuffer[0] * c3, 17) * c4;
+        p += 4;
+        ByteCount -= 4;
+      end;
+    2:
+      begin
+        h := RolDWord(h + FBuffer[0] * c3, 17) * c4;
+        h := RolDWord(h + FBuffer[1] * c3, 17) * c4;
+        p += 8;
+        ByteCount -= 8;
+      end;
+    3:
+      begin
+        h := RolDWord(h + FBuffer[0] * c3, 17) * c4;
+        h := RolDWord(h + FBuffer[1] * c3, 17) * c4;
+        h := RolDWord(h + FBuffer[2] * c3, 17) * c4;
+        p += 12;
+        ByteCount -= 12;
+      end;
+  end;
+  case ByteCount of
+    1:
+      h := RolDWord(h + DWord(p[0]) * c5, 11) * c1;
+    2:
+      begin
+        h := RolDWord(h + DWord(p[0]) * c5, 11) * c1;
+        h := RolDWord(h + DWord(p[1]) * c5, 11) * c1;
+      end;
+    3:
+      begin
+        h := RolDWord(h + DWord(p[0]) * c5, 11) * c1;
+        h := RolDWord(h + DWord(p[1]) * c5, 11) * c1;
+        h := RolDWord(h + DWord(p[2]) * c5, 11) * c1;
+      end;
+  end;
+  h := (h xor h shr 15) * c2;
+  h := (h xor h shr 13) * c3;
+  Result :=  h xor h shr 16;
+end;
 {$UNDEF c1}{$UNDEF c2}{$UNDEF c3}{$UNDEF c4}{$UNDEF c5}
 
 
@@ -506,7 +652,7 @@ begin
       end;
       if aCount >= 4 then
         begin
-          Result := RolQWord((Result xor QWord(PDWord(p)^)) * c1, 23) * c2 + c3;
+          Result := RolQWord(Result xor (QWord(PDWord(p)^) * c1), 23) * c2 + c3;
           p += 4;
           aCount -= 4;
         end;
@@ -591,7 +737,7 @@ begin
   end;
   if aCount >= 4 then
     begin
-      Result := RolQWord((Result xor QWord(PDWord(p)^)) * c1, 23) * c2 + c3;
+      Result := RolQWord(Result xor (QWord(PDWord(p)^) * c1), 23) * c2 + c3;
       p += 4;
       aCount -= 4;
     end;
@@ -633,7 +779,7 @@ end;
 
 class function TxxHash64LE.HashDWord(aValue: DWord; aSeed: QWord): QWord;
 begin
-  Result := RolQWord(((aSeed + c5 + 4) xor QWord(aValue)) * c1, 23) * c2 + c3;
+  Result := RolQWord((aSeed + c5 + 4) xor (QWord(aValue) * c1), 23) * c2 + c3;
   Result := (Result xor Result shr 33) * c2;
   Result := (Result xor Result shr 29) * c3;
   Result :=  Result xor Result shr 32;
@@ -662,6 +808,114 @@ begin
   Result :=  Result xor Result shr 32;
 end;
 {$ENDIF FPC_REQUIRES_PROPER_ALIGNMENT}
+
+{ TIncXxHash64LE }
+
+procedure TIncXxHash64LE.Reset(aSeed: QWord);
+begin
+  FV[0] := aSeed + c6;
+  FV[1] := aSeed + c2;
+  FV[2] := aSeed;
+  FV[3] := aSeed - c1;
+  FSeed := aSeed;
+  FTotalBytes := 0;
+  FHash := 0;
+  FBufIndex := 0;
+end;
+
+procedure TIncXxHash64LE.Add(const aBuffer; aCount: SizeInt);
+var
+  p: PByte;
+  ByteCount: Integer;
+begin
+  if aCount < 1 then exit;
+  Inc(FTotalBytes, aCount);
+  p := @aBuffer;
+  repeat
+    ByteCount := IntMin(SizeOf(FBuffer)-FBufIndex, aCount);
+    System.Move(p^, PByte(@FBuffer)[FBufIndex], ByteCount);
+    Dec(aCount, ByteCount);
+    Inc(FBufIndex, ByteCount);
+    Inc(p, ByteCount);
+    if FBufIndex = SizeOf(FBuffer) then
+      begin
+        FV[0] := RolQWord(FV[0] + FBuffer[0] * c2, 31) * c1;
+        FV[1] := RolQWord(FV[1] + FBuffer[1] * c2, 31) * c1;
+        FV[2] := RolQWord(FV[2] + FBuffer[2] * c2, 31) * c1;
+        FV[3] := RolQWord(FV[3] + FBuffer[3] * c2, 31) * c1;
+        FBufIndex := 0;
+      end;
+  until aCount = 0;
+end;
+
+function TIncXxHash64LE.Hash: QWord;
+var
+  p: PByte;
+  h: QWord;
+  ByteCount: Integer;
+begin
+  p := @FBuffer;
+  ByteCount := FBufIndex;
+  if FTotalBytes < SizeOf(FBuffer) then
+    h := FTotalBytes + FSeed + c5
+  else
+    begin
+      h := RolQWord(FV[0], 1) + RolQWord(FV[1], 7) + RolQWord(FV[2], 12) + RolQWord(FV[3], 18);
+      h := (h xor (RolQWord(FV[0] * c2, 31) * c1)) * c1 + c4;
+      h := (h xor (RolQWord(FV[1] * c2, 31) * c1)) * c1 + c4;
+      h := (h xor (RolQWord(FV[2] * c2, 31) * c1)) * c1 + c4;
+      h := (h xor (RolQWord(FV[3] * c2, 31) * c1)) * c1 + c4 + FTotalBytes;
+    end;
+  case ByteCount shr 3 of
+    1:
+      begin
+        h := RolQWord(h xor (c1 * RolQWord(FBuffer[0] * c2, 31)), 27) * c1 + c4;
+        ByteCount -= 8;
+        p += 8;
+      end;
+    2:
+      begin
+        h := RolQWord(h xor (c1 * RolQWord(FBuffer[0] * c2, 31)), 27) * c1 + c4;
+        h := RolQWord(h xor (c1 * RolQWord(FBuffer[1] * c2, 31)), 27) * c1 + c4;
+        ByteCount -= 16;
+        p += 16;
+      end;
+    3:
+      begin
+        h := RolQWord(h xor (c1 * RolQWord(FBuffer[0] * c2, 31)), 27) * c1 + c4;
+        h := RolQWord(h xor (c1 * RolQWord(FBuffer[1] * c2, 31)), 27) * c1 + c4;
+        h := RolQWord(h xor (c1 * RolQWord(FBuffer[2] * c2, 31)), 27) * c1 + c4;
+        ByteCount -= 24;
+        p += 24;
+      end;
+  end;
+  if ByteCount >= 4 then
+    begin
+      h := RolQWord(h xor (QWord(PDWord(p)^) * c1), 23) * c2 + c3;
+      p += 4;
+      ByteCount -= 4;
+    end;
+  case ByteCount of
+    1:
+      begin
+        h := RolQWord(h xor (QWord(p[0]) * c5), 11) * c1;
+      end;
+    2:
+      begin
+        h := RolQWord(h xor (QWord(p[0]) * c5), 11) * c1;
+        h := RolQWord(h xor (QWord(p[1]) * c5), 11) * c1;
+      end;
+    3:
+      begin
+        h := RolQWord(h xor (QWord(p[0]) * c5), 11) * c1;
+        h := RolQWord(h xor (QWord(p[1]) * c5), 11) * c1;
+        h := RolQWord(h xor (QWord(p[2]) * c5), 11) * c1;
+      end;
+  end;
+  h := (h xor h shr 33) * c2;
+  h := (h xor h shr 29) * c3;
+  Result :=  h xor h shr 32;
+end;
 {$UNDEF c1}{$UNDEF c2}{$UNDEF c3}{$UNDEF c4}{$UNDEF c5}{$UNDEF c6}
 
 { TMurmur2LE }
@@ -1207,12 +1461,93 @@ begin
   Result := (Result xor Result shr 13) * c5;
   Result := Result xor Result shr 16;
 end;
+
+{ TIncMurmur3LE }
+
+procedure TIncMurmur3LE.Reset(aSeed: DWord);
+begin
+  FTotalBytes := 0;
+  FHash := aSeed;
+  FBufIndex := 0;
+end;
+
+procedure TIncMurmur3LE.Add(const aBuffer; aCount: SizeInt);
+var
+  p: PByte;
+  h: DWord;
+  ByteCount: Integer;
+begin
+  if aCount < 1 then exit;
+  Inc(FTotalBytes, aCount);
+  p := @aBuffer;
+  repeat
+    ByteCount := IntMin(SizeOf(FBuffer)-FBufIndex, aCount);
+    System.Move(p^, PByte(@FBuffer)[FBufIndex], ByteCount);
+    Dec(aCount, ByteCount);
+    Inc(FBufIndex, ByteCount);
+    Inc(p, ByteCount);
+    if FBufIndex = SizeOf(FBuffer) then
+      begin
+        h := RolDWord(FHash xor RolDWord(FBuffer[0] * c1, 15) * c2, 13) * 5 + c3;
+        h := RolDWord(h xor RolDWord(FBuffer[1] * c1, 15) * c2, 13) * 5 + c3;
+        h := RolDWord(h xor RolDWord(FBuffer[2] * c1, 15) * c2, 13) * 5 + c3;
+        FHash := RolDWord(h xor RolDWord(FBuffer[3] * c1, 15) * c2, 13) * 5 + c3;
+        FBufIndex := 0;
+      end;
+  until aCount = 0;
+end;
+
+function TIncMurmur3LE.Hash: DWord;
+type
+  TByte3 = array[0..2] of Byte;
+  PByte3 = ^TByte3;
+var
+  p: PByte;
+  h, k: DWord;
+  ByteCount: Integer;
+begin
+  p := @FBuffer;
+  ByteCount := FBufIndex;
+  h := FHash;
+  case ByteCount shr 2 of
+    1:
+      begin
+        h := RolDWord(h xor RolDWord(FBuffer[0] * c1, 15) * c2, 13) * 5 + c3;
+        p += 4;
+        ByteCount -= 4;
+      end;
+    2:
+      begin
+        h := RolDWord(h xor RolDWord(FBuffer[0] * c1, 15) * c2, 13) * 5 + c3;
+        h := RolDWord(h xor RolDWord(FBuffer[1] * c1, 15) * c2, 13) * 5 + c3;
+        p += 8;
+        ByteCount -= 8;
+      end;
+    3:
+      begin
+        h := RolDWord(h xor RolDWord(FBuffer[0] * c1, 15) * c2, 13) * 5 + c3;
+        h := RolDWord(h xor RolDWord(FBuffer[1] * c1, 15) * c2, 13) * 5 + c3;
+        h := RolDWord(h xor RolDWord(FBuffer[2] * c1, 15) * c2, 13) * 5 + c3;
+        p += 12;
+        ByteCount -= 12;
+      end;
+  end;
+  k := 0;
+  case ByteCount of
+    1: PByte(@k)^  := PByte(p)^;
+    2: PWord(@k)^  := PWord(p)^;
+    3: PByte3(@k)^ := PByte3(p)^;
+  end;
+  h := h xor (RolDWord(k * c1, 15) * c2) xor FTotalBytes;
+  h := (h xor h shr 16) * c4;
+  h := (h xor h shr 13) * c5;
+  Result := h xor h shr 16;
+end;
 {$UNDEF c1}{$UNDEF c2}{$UNDEF c3}{$UNDEF c4}{$UNDEF c5}
 
 { TMurmur64aLE }
 
 {$DEFINE m64 := QWord($c6a4a7935bd1e995)}
-
 {$IFDEF FPC_REQUIRES_PROPER_ALIGNMENT}{$PUSH}{$WARN 5057 OFF}
 class function TMurmur64aLE.HashBufUnalign(aBuffer: Pointer; aCount: SizeInt; aSeed: QWord): QWord;
 var
